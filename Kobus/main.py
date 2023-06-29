150354c5a51c2069dd4b133cc8ae52c2c61ce185
Kobus <kobus.oberholzer@codehesion.co.za>
2023-06-29 16:30:45 +0200

    Prime Number generator

--- Kobus/main.py
+++ Kobus/main.py
@@ -1,40 +1,17 @@
+import sympy
+import requests
 import time
 import math
 import random
-import asyncio
-import httpx
-from concurrent.futures import ThreadPoolExecutor
+import json
+import multiprocessing
 
 class Benchmark:
 
     @staticmethod
-    def do_work(random_generator):
-        x, y, z = [random_generator.randint(0, 1000) for _ in range(3)]
-        length = math.sqrt(x * x + y * y + z * z)
-        x_unit, y_unit, z_unit = x / length, y / length, z / length
-        length_unit = math.sqrt(x_unit**2 + y_unit**2 + z_unit**2)
-
-        if abs(length_unit - 1) > 0.0003:
-            raise Exception(f"Unit Variable Calculation is wrong {length_unit} from {x},{y},{z} to {x_unit},{y_unit},{z_unit}")
-
-        return [x, y, z]
-
-    @staticmethod
-    async def verify_algorithm_output(algorithm_output):
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                "https://hackathon-validator.vercel.app/api/verify",
-                json=algorithm_output
-            )
-            response.raise_for_status()
-            result = response.json()
-            return result["success"]
-
-    @staticmethod
-    async def calculate_benchmark(time_in_seconds, results):
-        random_generator = random.Random()
-        algorithm_result = Benchmark.do_work(random_generator)
-        is_successful_algorithm = await Benchmark.verify_algorithm_output(algorithm_result)
+    def calculate_benchmark(time_in_seconds, results):
+        algorithm_result = Benchmark.do_work()
+        is_successful_algorithm = Benchmark.verify_algorithm_output(algorithm_result)
         if not is_successful_algorithm:
             raise Exception("The value should be positive")
 
@@ -42,31 +19,56 @@ class Benchmark:
         start_time = time.time()
 
         while time.time() - start_time < time_in_seconds:
-            Benchmark.do_work(random_generator)
+            Benchmark.do_work()
             counter += 1
 
         total_seconds = time.time() - start_time
-        results.append((counter, total_seconds))
+        results.put((counter, total_seconds))
 
-async def main():
-    # Shared list for storing results
-    results = []
+    @staticmethod
+    def do_work():
+        l_limit = 0
+        h_limit = 1000000         
+        prime_numbers = list(sympy.primerange(l_limit,h_limit+1))
 
-    # Create tasks for asyncio.gather
-    tasks = [Benchmark.calculate_benchmark(5, results) for _ in range(4)]
+        return prime_numbers
 
-    # Run all tasks and wait for them to complete
-    await asyncio.gather(*tasks)
+    @staticmethod
+    def verify_algorithm_output(algorithm_output):
+        response = requests.post(
+            "https://hackathon-validator.vercel.app/api/verify",
+            json=algorithm_output
+        )
+        response.raise_for_status()
+        result = response.json()
+        print(result)
+        return result["success"]
+
+def main():
+    # Shared queue for storing results
+    results = multiprocessing.Manager().Queue()
+
+    # Create worker processes
+    processes = [multiprocessing.Process(target=Benchmark.calculate_benchmark, args=(5, results)) for _ in range(4)]
+
+    # Start all processes
+    for p in processes:
+        p.start()
+
+    # Wait for all processes to finish
+    for p in processes:
+        p.join()
 
     # Summarize results
     total_counts = 0
     total_seconds = 0
-    for count, seconds in results:
+    for _ in range(4):
+        count, seconds = results.get()
         total_counts += count
         total_seconds += seconds
 
     print(f"{total_counts};{total_seconds / 4};")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
 
