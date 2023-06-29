0d65dec3ff03de128095d64df8a1524e1f181d5b
Jaco1998 <jaco@codehesion.co.za>
2023-06-29 10:54:18 +0200

    setup of node project

--- Jaco/src/app.js
+++ Jaco/src/app.js
@@ -1,9 +1,31 @@
-const program = new (require('./program'))(); 
+'use strict'
+
+import fetch from 'node-fetch';
+import Program from './program.js'
+
+const program = new Program();
+
+const APIURL = "https://hackathon-validator.vercel.app/api/verify";
+
+async function start (){
+    let intArray = [];
+    for(var i = 0 ; i < 5 ; i ++ ){
+        intArray.push(Math.random(new Date().getTime() ));
+    }
+    const string = JSON.stringify(intArray)
+    await fetch(APIURL , {
+        method:'POST',
+        body: string,
+        headers: {
+            'ContentType':'application/json',
+            "Content-length": string.length
+        }
+    } )
+    .then((result)=>{
+        console.log(result.status);
+        program.start();
+    }) 
+}
 
 
-program.run();
-
-setTimeout(()=>{
-    program.stop();
-    console.log(program.getItterations())
-} , 5000);
+start ();
--- Jaco/src/program.js
+++ Jaco/src/program.js
@@ -3,19 +3,30 @@
 class Program {
     constructor() {
         this.itterations = 0;
+        this.startTime = null;
+        this.endTime = null;
+        this.timeUp = false;
+        this.timeOut = null;
+    }
+
+    start() {
+        this.startTime = new Date().getTime();
+        this.run();      
     }
 
     run () {
-
+        var etime =null;
+        do{
+            this.itterations++;
+            etime = new Date().getTime();
+        }while((etime - this.startTime ) < 5000);
+        this.endTime = etime;
+        this.stop();
     }
 
     stop(){
-
-    }
-
-    getItterations (){
-        return this.itterations;
+        console.log(`${this.itterations},${(this.endTime - this.startTime)/1000}`)
     }
 }
 
-module.exports = Program;
+export default  Program;
